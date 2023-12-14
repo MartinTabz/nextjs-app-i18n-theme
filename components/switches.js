16456be89201eeec5788741e1b7a5fb3aa5e3524
Martin Šíl <mail@martinsil.cz>
2023-12-14 23:24:11 +0100

    feat: language switch

--- components/switches.js
+++ components/switches.js
@@ -1,8 +1,13 @@
 "use client";
 
+import locales, { flags } from "@translation/locales";
 import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function Switches() {
+export default function Switches({ locale: currentLocale }) {
+	const [localeOpened, setLocaleOpened] = useState(false);
 	const { resolvedTheme, setTheme } = useTheme();
 
 	return (
@@ -12,6 +17,34 @@ export default function Switches() {
 			>
 				{resolvedTheme === "dark" ? "light" : "dark"}
 			</button>
+			<div>
+				<div onClick={() => setLocaleOpened(!localeOpened)}>
+					<Image
+						src={flags.find((flag) => flag.locale === currentLocale).flag}
+						alt=""
+						width={50}
+						height={32}
+					/>
+				</div>
+				<div
+					style={{
+						display: localeOpened ? "flex" : "none",
+						flexDirection: "column",
+					}}
+				>
+					{locales.map((locale, index) => {
+						if (locale !== currentLocale) {
+							const flagInfo = flags.find((flag) => flag.locale === locale);
+							return (
+								<Link key={index} href={`/${locale}`} locale={locale}>
+									<Image width={50} height={32} src={flagInfo.flag} alt="" />
+								</Link>
+							);
+						}
+						return null;
+					})}
+				</div>
+			</div>
 		</div>
 	);
 }
--- translation/locales.js
+++ translation/locales.js
@@ -1,2 +1,11 @@
+import czvlajka from "@public/czvlajka.png";
+import devlajka from "@public/devlajka.png";
+import envlajka from "@public/envlajka.png";
+
 export default ["cs", "en", "de"];
- 
+
+export const flags = [
+	{ locale: "cs", flag: czvlajka },
+	{ locale: "en", flag: envlajka },
+	{ locale: "de", flag: devlajka },
+];
